f387ebee2e3be167d414e51af547e7af3b9eeac4
Javier <javierfelipe94@gmail.com>
2018-11-19 17:50:49 +0100

    Front page redesign with icons.

--- front-page.php
+++ front-page.php
@@ -55,19 +55,18 @@ get_header();
 
     ?>
     <div class="about-us" id="about">
-        <h2 class="main-title">M&amp;L Burger Haus, pronto en Donostia.</h2>
-        <div class="col">                        
-            <div class="about-text">
-                <p>M&amp;L BurgerHaus nace con la intención de acercar esta ciudad, tan dada a la buena gastronomía, a un producto que en los últimos años ha dejado de ser considerado comida basura y se ha convertido en una comida saludable y gourmet.</p>
-                <p>M&amp;L es la idea de dos jóvenes vascos entusiasmados por la buena gastronomía y las hamburguesas. Tras probar hamburguesas en decenas de países decidieron seguir su pasión y abrir un restaurante mezclando dos de sus grandes placeres: las hamburguesas y la cerveza.</p>
-            </div>
-        </div>                               
-        <div class="col">                        
-            <div class="about-text">
-                <p>Nuestra idea es enseñar al público donostiarra que la hamburguesa puede ser también un plato único y especial, con el que se puede innovar y obtener unos sabores auténticos. </p>
-                <p>Por ello ofrecemos unas hamburguesas diferentes a cualquier otra del mercado, con unos sabores vivos y una jugosidad y frescura que no se encuentra en otra parte. Todo ello utilizando productos de la tierra, ya que siempre hemos creído en promover el producto local y aprovechar su alta calidad.</p>
-                <p>En M&amp;L no nos olvidamos de nadie y por ello ofrecemos opciones para todo tipo de gustos: hamburguesas de ternera, de pollo y vegetarianas. Siendo esta último de un concepto que nada tiene que ver con el resto de hamburguesas vegetarianas del mercado.</p>                        
-            </div>                         
+        <h2 class="main-title">M&amp;L Burger Haus, pronto en Donostia.</h2>                             
+        <div class="about-row">                        
+            <p>M&amp;L BurgerHaus nace con la intención de acercar esta ciudad, tan dada a la buena gastronomía, a un producto que en los últimos años ha dejado de ser considerado comida basura y se ha convertido en una comida saludable y gourmet.</p>
+            <div class="img-container"><img src="https://mlburgerhaus.com/wp-content/uploads/2018/11/icons8-hamburger-80.png" alt="burger-image"/></div>                      
+        </div>
+        <div class="about-row">                                  
+            <p>La hamburguesa es el eje de nuestro local, pero ni mucho menos el único producto que ofrecemos. Alitas de pollo al auténtico estilo americano, suculentos postres caseros y una variada carta de cervezas artesanales son algunas de nuestras propuestas adicionales.</p>
+            <div class="img-container"><img src="https://mlburgerhaus.com/wp-content/uploads/2018/11/icons8-beer-glass-64.png" alt="beer-image"/></div>                      
+        </div>
+        <div class="about-row">                        
+            <p>En M&amp;L no nos olvidamos de nadie y por ello ofrecemos opciones para todo tipo de gustos: hamburguesas de ternera, de pollo y vegetarianas. Todo ello utilizando productos locales y de alta calidad. </p>
+            <div class="img-container"><img src="https://mlburgerhaus.com/wp-content/uploads/2018/11/icons8-natural-food-64.png" alt="healthy-food-image"/></div>                    
         </div>
     </div>
     <div class="photo-carousel">
